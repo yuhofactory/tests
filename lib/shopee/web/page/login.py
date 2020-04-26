facb4a39820371784860a8a9bd2ac4d579297344
Hisyamuddin Jamirun <hisyamuddin.jamirun@gmail.com>
2020-04-27 01:16:15 +0800

    Add logging for shopee login webpage

--- lib/shopee/web/page/login.py
+++ lib/shopee/web/page/login.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import traceback
+import log
 
 page = {
 	"name"         : "login",
@@ -17,6 +18,7 @@ page = {
 username_input_xpath = "//input[@class='shopee-input__input' and @placeholder='Email/Phone/Username']"
 password_input_xpath = "//input[@class='shopee-input__input' and @placeholder='Password']"
 login_button_xpath = "//button[descendant::span[text()='Log In']]"
+login_log = log.get_logger(logger_name="lib.shopee.web.page.login", logging_level="info")
 
 #################################################
 #                 Navigations                   #
@@ -25,9 +27,11 @@ def enter_username(webdriver, wait_event, username):
 	try:
 		username_input = wait_event.until(ec.visibility_of_element_located((By.XPATH, username_input_xpath)))
 		username_input.send_keys(username)
+		login_log.debug("Successfully enter username")
 		return True
 
 	except:
+		login_log.error("Failed to enter username")
 		print(traceback.format_exc())
 		return False
 
@@ -35,9 +39,11 @@ def enter_password(webdriver, wait_event, password):
 	try:
 		password_input = wait_event.until(ec.visibility_of_element_located((By.XPATH, password_input_xpath)))
 		password_input.send_keys(password)
+		login_log.debug("Successfully enter password")
 		return True
 
 	except:
+		login_log.error("Failed to enter password")
 		print(traceback.format_exc())
 		return False
 
@@ -45,15 +51,25 @@ def click_login_button(webdriver, wait_event):
 	try:
 		login_button = wait_event.until(ec.element_to_be_clickable((By.XPATH, login_button_xpath)))
 		login_button.click()
+		login_log.debug("Successfully click login button")
 		return True
 
 	except:
+		login_log.error("Failed to click login button")
 		print(traceback.format_exc())
 		return False
 
 def login(webdriver, username, password, waiting_time=30):
 	webdriver.get("https://seller.shopee.com.my")
 	wait_event = WebDriverWait(webdriver, waiting_time)
-	enter_username(webdriver, wait_event, username)
-	enter_password(webdriver, wait_event, password)
-	click_login_button(webdriver, wait_event)
+	status = []
+	status.append(enter_username(webdriver, wait_event, username))
+	status.append(enter_password(webdriver, wait_event, password))
+	status.append(click_login_button(webdriver, wait_event))
+
+	if False in status:
+		login_log.error("Failed to login")
+		return False
+	else:
+		login_log.info("Successfully login")
+		return True
