b85e569d07c525a92f713d709a5b4c750e456839
Hisyamuddin Jamirun <hisyamuddin.jamirun@gmail.com>
2020-05-02 00:15:05 +0800

    Initial version resource for stock amount input file

---
+++ lib/resource/stock.py
@@ -0,0 +1,44 @@
+import traceback
+import log
+import os
+import pandas
+import shopee.config as config
+
+conf = config.get_config()
+stock_log = log.get_logger(logger_name="lib.resource.stock", logging_level=conf.get("LOGGING", "LEVEL"))
+stock_amount_file_path = "{}/input_file/stock_amount.csv".format(os.path.dirname(os.path.realpath(__file__)))
+
+def get_stock_amount():
+	if os.path.exists(stock_amount_file_path):
+		try:
+			df_header = pandas.read_csv(stock_amount_file_path, nrows=1)
+			df_header_list = list(df_header.columns)
+			df_stock_amount = pandas.read_csv(stock_amount_file_path, \
+				names=df_header_list, usecols=df_header_list, skiprows=1)
+			stock_amount_list = []
+
+			for i in df_stock_amount.index:
+				ommohome_product = str(df_stock_amount.loc[i, 'ommohome_product'])
+				shopee_product = str(df_stock_amount.loc[i, 'shopee_product'])
+				size = str(df_stock_amount.loc[i, 'size'])
+				colour = str(df_stock_amount.loc[i, 'colour'])
+				previous_amount = str(df_stock_amount.loc[i, 'previous_amount'])
+				current_amount = str(df_stock_amount.loc[i, 'current_amount'])
+
+				stock_amount_list.append([
+					ommohome_product, 
+					shopee_product, 
+					size, 
+					colour, 
+					previous_amount,
+					current_amount
+				])
+
+			stock_log.info("Successfully retrieved stock amount")
+
+		except:
+			stock_log.error(traceback.print_exc())
+	else:
+		stock_log.error("Invalid path for stock amount file : {}".format(stock_amount_file_path))
+
+	return stock_amount_list
