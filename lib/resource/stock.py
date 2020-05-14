d91043a392357207f9944adc45a78fd2d7c40be6
Hisyamuddin Jamirun <hisyamuddin.jamirun@gmail.com>
2020-05-14 18:56:04 +0800

    Fix logical error of read write operations for stock amount input file

--- lib/resource/stock.py
+++ lib/resource/stock.py
@@ -14,74 +14,73 @@ def get_stock_amount(product_index_name):
 	if os.path.exists(stock_amount_temp_path) == False:
 		shutil.copy2(stock_amount_repo_path, stock_amount_temp_path)
 
-	else:
-		try:
-			df_header = pandas.read_csv(stock_amount_temp_path, nrows=1)
-			df_header_list = list(df_header.columns)
-			df_stock_amount = pandas.read_csv(stock_amount_temp_path, \
-				names=df_header_list, usecols=df_header_list, skiprows=1, keep_default_na=False)
-			stock_amount_2Dlist = []
-			stock_amount_3Dlist = []
-			product_name = str(df_stock_amount.loc[0, product_index_name])
+	try:
+		df_header = pandas.read_csv(stock_amount_temp_path, nrows=1)
+		df_header_list = list(df_header.columns)
+		df_stock_amount = pandas.read_csv(stock_amount_temp_path, \
+			names=df_header_list, usecols=df_header_list, skiprows=1, keep_default_na=False)
+		stock_amount_2Dlist = []
+		stock_amount_3Dlist = []
+		product_name = str(df_stock_amount.loc[0, product_index_name])
 
-			for i in df_stock_amount.index:
-				ommohome_product_name = str(df_stock_amount.loc[i, "ommohome_product_name"])
-				ommohome_variation_type = str(df_stock_amount.loc[i, "ommohome_variation_type"])
-				ommohome_variation_data = str(df_stock_amount.loc[i, "ommohome_variation_data"])
-				shopee_product_name = str(df_stock_amount.loc[i, "shopee_product_name"])
-				shopee_variation_type = str(df_stock_amount.loc[i, "shopee_variation_type"])
-				shopee_variation_data = str(df_stock_amount.loc[i, "shopee_variation_data"])
-				previous_amount = str(df_stock_amount.loc[i, "previous_amount"])
-				current_amount = str(df_stock_amount.loc[i, "current_amount"])
+		for i in df_stock_amount.index:
+			ommohome_product_name = str(df_stock_amount.loc[i, "ommohome_product_name"])
+			ommohome_variation_type = str(df_stock_amount.loc[i, "ommohome_variation_type"])
+			ommohome_variation_data = str(df_stock_amount.loc[i, "ommohome_variation_data"])
+			shopee_product_name = str(df_stock_amount.loc[i, "shopee_product_name"])
+			shopee_variation_type = str(df_stock_amount.loc[i, "shopee_variation_type"])
+			shopee_variation_data = str(df_stock_amount.loc[i, "shopee_variation_data"])
+			previous_amount = str(df_stock_amount.loc[i, "previous_amount"])
+			current_amount = str(df_stock_amount.loc[i, "current_amount"])
 
-				if product_index_name == "shopee_product_name":
-					if product_name == shopee_product_name:
-						stock_amount_2Dlist.append([
-							shopee_product_name, 
-							shopee_variation_type, 
-							shopee_variation_data, 
-							previous_amount,
-							current_amount
-						])
-					else:
-						stock_amount_3Dlist.append(stock_amount_2Dlist)
-						stock_amount_2Dlist = []
-						stock_amount_2Dlist.append([
-							shopee_product_name, 
-							shopee_variation_type, 
-							shopee_variation_data, 
-							previous_amount,
-							current_amount
-						])
-						product_name = shopee_product_name
+			if product_index_name == "shopee_product_name":
+				if product_name == shopee_product_name:
+					stock_amount_2Dlist.append([
+						shopee_product_name, 
+						shopee_variation_type, 
+						shopee_variation_data, 
+						previous_amount,
+						current_amount
+					])
+				else:
+					stock_amount_3Dlist.append(stock_amount_2Dlist)
+					stock_amount_2Dlist = []
+					stock_amount_2Dlist.append([
+						shopee_product_name, 
+						shopee_variation_type, 
+						shopee_variation_data, 
+						previous_amount,
+						current_amount
+					])
+					product_name = shopee_product_name
 
-				elif product_index_name == "ommohome_product_name":
-					if product_name == ommohome_product_name:
-						stock_amount_2Dlist.append([
-							ommohome_product_name, 
-							ommohome_variation_type, 
-							ommohome_variation_data, 
-							previous_amount,
-							current_amount
-						])
-					else:
-						stock_amount_3Dlist.append(stock_amount_2Dlist)
-						stock_amount_2Dlist = []
-						stock_amount_2Dlist.append([
-							ommohome_product_name, 
-							ommohome_variation_type, 
-							ommohome_variation_data, 
-							previous_amount,
-							current_amount
-						])
-						product_name = ommohome_product_name
+			elif product_index_name == "ommohome_product_name":
+				if product_name == ommohome_product_name:
+					stock_amount_2Dlist.append([
+						ommohome_product_name, 
+						ommohome_variation_type, 
+						ommohome_variation_data, 
+						previous_amount,
+						current_amount
+					])
+				else:
+					stock_amount_3Dlist.append(stock_amount_2Dlist)
+					stock_amount_2Dlist = []
+					stock_amount_2Dlist.append([
+						ommohome_product_name, 
+						ommohome_variation_type, 
+						ommohome_variation_data, 
+						previous_amount,
+						current_amount
+					])
+					product_name = ommohome_product_name
 
-			# Append the last 2D list
-			stock_amount_3Dlist.append(stock_amount_2Dlist)
-			stock_log.info("Successfully retrieved stock amount")
+		# Append the last 2D list
+		stock_amount_3Dlist.append(stock_amount_2Dlist)
+		stock_log.info("Successfully retrieved stock amount")
 
-		except:
-			stock_log.error(traceback.print_exc())
+	except:
+		stock_log.error(traceback.print_exc())
 
 	return stock_amount_3Dlist
 
@@ -89,20 +88,17 @@ def set_stock_amount(stock_amount_list):
 	if os.path.exists(stock_amount_temp_path) == False:
 		shutil.copy2(stock_amount_repo_path, stock_amount_temp_path)
 
-	else:
-		try:
-			df_header = pandas.read_csv(stock_amount_temp_path, nrows=1)
-			df_header_list = list(df_header.columns)
-			df_stock_amount = pandas.read_csv(stock_amount_temp_path, \
-				names=df_header_list, usecols=df_header_list, skiprows=1, keep_default_na=False)
+	try:
+		df_header = pandas.read_csv(stock_amount_temp_path, nrows=1)
+		df_header_list = list(df_header.columns)
+		df_stock_amount = pandas.read_csv(stock_amount_temp_path, \
+			names=df_header_list, usecols=df_header_list, skiprows=1, keep_default_na=False)
 
-			for i in df_stock_amount.index:
-				df_stock_amount.loc[i, "previous_amount"] = str(df_stock_amount.loc[i, "current_amount"])
-				df_stock_amount.loc[i, "current_amount"] = stock_amount_list[i]
+		for i in df_stock_amount.index:
+			df_stock_amount.loc[i, "previous_amount"] = str(df_stock_amount.loc[i, "current_amount"])
+			df_stock_amount.loc[i, "current_amount"] = stock_amount_list[i]
 
-			df_stock_amount.to_csv(stock_amount_temp_path, index=False, encoding='utf8')
+		df_stock_amount.to_csv(stock_amount_temp_path, index=False, encoding='utf8')
 
-		except:
-			stock_log.error(traceback.print_exc())
-	
-	
+	except:
+		stock_log.error(traceback.print_exc())
