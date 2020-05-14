b765150e6d22f15d3236ab5aca712152bbf1a9eb
Hisyamuddin Jamirun <hisyamuddin.jamirun@gmail.com>
2020-05-14 18:58:31 +0800

    Add workaround for selecting product variation in ommohome product webpage

--- lib/ommohome/web/page/product.py
+++ lib/ommohome/web/page/product.py
@@ -22,11 +22,12 @@ page = {
 conf = config.get_config()
 product_log = log.get_logger(logger_name="lib.ommohome.web.page.product", logging_level=conf.get("LOGGING", "LEVEL"))
 
-variation_dropdown_xpath = "//table[@class='variations']//descendant::td[child::label[@for='{}']]\
-	//following-sibling::td//descendant::select[@id='{}']"
+variation_dropdown_xpath = "//table[@class='variations']//descendant::td[child::label[contains(text(), '{}')]]\
+	//following-sibling::td//descendant::select"
 variation_dropdown_item_xpath = "{}/option[@value='{}']"
 standard_policy_label_xpath = "//div[@class='entry-summary-content']//descendant::div[@class='woo-short-description']\
-	//descendant::p[last()]"
+	//descendant::p[contains(text(), 'Standard policy')]//descendant::br[last()]"
+product_name_label_xpath = "//h2[contains(@class, 'product_title entry-title')]"
 stock_amount_xpath = "//p[contains(@class, 'stock') and contains(text(), 'Availability')]//descendant::span"
 
 OMMOHOME_PRODUCT_NAME_INDEX = 0
@@ -41,10 +42,19 @@ CURRENT_AMOUNT_INDEX = 4
 def select_product_variation(webdriver, product_variation_2Dlist, waiting_time=30):
 	try:
 		wait_event = WebDriverWait(webdriver, waiting_time)
-		standard_policy_label = wait_event.until(ec.visibility_of_element_located(\
-			(By.XPATH, standard_policy_label_xpath)))
-		# Scroll down as close as possible to the product variation dropdown to ensure its visibility
-		standard_policy_label.location_once_scrolled_into_view
+
+		try:
+			standard_policy_label = wait_event.until(ec.visibility_of_element_located(\
+				(By.XPATH, standard_policy_label_xpath)))
+			# Scroll down as close as possible to the product variation dropdown to ensure its visibility
+			standard_policy_label.location_once_scrolled_into_view
+
+		except:
+			# Workaround for scroll action if standard policy label doesn't exist
+			product_name_label = wait_event.until(ec.visibility_of_element_located(\
+				(By.XPATH, product_name_label_xpath)))
+			product_name_label.location_once_scrolled_into_view
+
 		time.sleep(1)
 		stock_amount_list = []
 
@@ -56,14 +66,12 @@ def select_product_variation(webdriver, product_variation_2Dlist, waiting_time=3
 
 				for i in range(len(variation_type_list)):
 					variation_dropdown = wait_event.until(ec.element_to_be_clickable(\
-						(By.XPATH, variation_dropdown_xpath.format(variation_type_list[i].lower(), \
-							variation_type_list[i].lower()))))
+						(By.XPATH, variation_dropdown_xpath.format(variation_type_list[i]))))
 					variation_dropdown.click()
 
 					variation_dropdown_item = wait_event.until(ec.element_to_be_clickable(\
 						(By.XPATH, variation_dropdown_item_xpath.format(variation_dropdown_xpath.format(\
-							variation_type_list[i].lower(), variation_type_list[i].lower()), \
-							variation_data_list[i]))))
+							variation_type_list[i]), variation_data_list[i]))))
 					variation_dropdown_item.click()
 
 					product_log.info("Successfully selected variation {} {} for product {}".format(\
