0219684cf20f7bf92869f231f80b035d0547f405
Hisyamuddin Jamirun <hisyamuddin.jamirun@gmail.com>
2020-05-19 12:54:17 +0800

    Set filepath for configuration data module

--- lib/resource/config.py
+++ lib/resource/config.py
@@ -3,6 +3,12 @@ import os
 
 def get_config():
 	config = SafeConfigParser()
-	home_dir = os.getenv("HOME")
-	config.read("{}/.tests_conf/config_resource.conf".format(home_dir))
+	config_filename = "config_resource.conf"
+	config_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), config_filename)
+
+	if os.path.exists(config_filepath) == False:
+		config_filepath = os.path.join(os.getenv("HOME"), ".tests_conf", config_filename)
+
+	config.read(config_filepath)
+
 	return config
